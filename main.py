9338f1cd9cc6b9ab4f458547398f3586ce3ea725
Himanshu Kesharwani <sunnychoudharyql@gmail.com>
2025-08-24 12:35:14 +0530

    [FEAT]: Added Post API request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Body
 #from pydantic import BaseModel
 
 app = FastAPI()
@@ -8,6 +9,29 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"data": "Welecome to FastAPI! This is a simple API built with FastAPI."}
+
+
+@app.get("/notes")
+async def read_notes():
+    return {"notes": ["Note 1", "Note 2", "Note 3"]}
+
+
+@app.post("/create_note")
+async def create_note(note: str, category: str):
+    print(f"Creating note: {note} with category: {category}")
+    return {"message": f"Note '{note}' with category '{category}' created successfully!"}
+
+@app.post("/create_notes")
+async def create_note(payload: dict = Body(...)):
+    print(payload)
+    return {"message": f"Note with params '{payload}' created successfully!"}
+    
+
+
+@app.post("/create_category")
+async def create_category(category: str):
+    print(f"Creating category: {category}")
+    return {"message": f"Category '{category}' created successfully!"}  
 
 
 
